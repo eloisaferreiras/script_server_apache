5f2c25c0c089fe81c6fb7f96141f08c983ca173a
eloisaferreiras <alwayzasuzana17mp3@gmail.com>
2023-11-29 20:50:15 +0000

    files

--- script-iac2.sh
+++ script-iac2.sh
@@ -6,7 +6,7 @@ apt-get upgrade -y
 apt-get install apache2 -y 
 apt-get install unzip -y 
 
-echo "Baixando e copiando os arquivos da aplicação.." 
+echo "Baixando e copiando os arquivos da aplicação..." 
 
 cd /tmp 
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
